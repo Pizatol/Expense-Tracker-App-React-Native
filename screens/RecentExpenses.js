8fde80fe91b6b89d10ab41df2c6913f4171b4824
Pizatol <dev.belmonte@gmail.com>
2022-06-03 18:53:11 +0200

    first commit

---
+++ screens/RecentExpenses.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function RecentExpenses() {
+  return (
+	 <View>
+		<Text>Recent Expenses screen</Text>
+	 </View>
+  )
+}
+
+const styles = StyleSheet.create({})

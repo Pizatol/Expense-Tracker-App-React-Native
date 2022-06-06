f8409c246c60a1d5822e1944d1c68122d9331854
Pizatol <dev.belmonte@gmail.com>
2022-06-06 23:22:06 +0200

    mise en place des fonctions GET et POST via Axios

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,31 +1,36 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React,  { useContext, useEffect } from 'react'
-import ExpensesOutput from '../components/ExpensesOutput/ExpensesOutput'
+import { StyleSheet, Text, View } from "react-native";
+import React, { useContext, useEffect } from "react";
+import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
 
-import { ExpensesContext } from '../store/expenses-context'
-import { getDateMinusDays } from '../util/date';
-import { fetchExpenses } from '../util/https';
+import { ExpensesContext } from "../store/expenses-context";
+import { getDateMinusDays } from "../util/date";
+import { fetchExpenses } from "../util/https";
 
 export default function RecentExpenses() {
-  const expensesCtx = useContext(ExpensesContext);
+    const expensesCtx = useContext(ExpensesContext);
 
-  useEffect(() => {
-    fetchExpenses();
-  }, [])
+    useEffect(() => {
+        async function getExpenses() {
+          const expenses =  await fetchExpenses();
+        }
+        getExpenses();
+    }, []);
 
-  const recentExpenses =expensesCtx.expenses.filter((expense) => {
-    const today = new Date();
+    const recentExpenses = expensesCtx.expenses.filter((expense) => {
+        const today = new Date();
 
-    const date7daysAgo = getDateMinusDays(today, 7);
+        const date7daysAgo = getDateMinusDays(today, 7);
 
-    return (expense.date > date7daysAgo) && (expense.date <= today);
+        return expense.date > date7daysAgo && expense.date <= today;
+    });
 
-  })
-
-
-  return (
-	 <ExpensesOutput fallbackText="No expenses registered for the last 7 days." expenses={recentExpenses} expensesPeriod='Last 7 days' />
-  )
+    return (
+        <ExpensesOutput
+            fallbackText="No expenses registered for the last 7 days."
+            expenses={recentExpenses}
+            expensesPeriod="Last 7 days"
+        />
+    );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
--- util/https.js
+++ util/https.js
@@ -10,7 +10,19 @@ export function storeExpense(expenseData){
 }
 
 export async function fetchExpenses() {
- const response = await	axios.get(BACKEND_URL + "/expenses.json",
+ const response = await	axios.get(BACKEND_URL + "/expenses.json");
 
-	)
+	const expenses = [];
+
+	for(const key in response.data) {
+		const expenseObj = {
+			id : key,
+			amount : response.data[key].amount,
+			date : new Date(response.data[key].date) ,
+			description : response.data[key].description
+		}
+		expenses.push(expenseObj)
+
+	}
+	return expenses;
 }
